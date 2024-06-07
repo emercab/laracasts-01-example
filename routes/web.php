67b4c39a356c2dfdfbfd142351d47f9a06281445
Emerson Cabrera <info@emersoncabrera.com>
2024-06-06 23:49:39 -0500

    Form components have been created.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,10 @@
 <?php
 
 use App\Http\Controllers\JobController;
-use App\Models\Job;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::controller(JobController::class)->group(function () {
-  Route::get('/jobs', 'index');
-  Route::get('/jobs/create', 'create');
-  Route::post('/jobs', 'store');
-  Route::get('/jobs/{job}/edit', 'edit');
-  Route::patch('/jobs/{job}', 'update');
-  Route::get('/jobs/{job}', 'show');
-  Route::delete('/jobs/{job}', 'destroy');
-});
+Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
